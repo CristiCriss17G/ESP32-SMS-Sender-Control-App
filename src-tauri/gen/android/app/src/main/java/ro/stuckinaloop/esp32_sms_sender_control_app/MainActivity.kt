bdd062b9e0adac6dc5b438c528765fd14f0e749d
Cristian Iordachescu <iordachescu1996@outlook.com>
2025-09-29 21:02:51 +0300

    feat(android): initialize Android project structure and resources
    - Added MainActivity.kt to set up the main activity with edge-to-edge support.
    - Created drawable resources for launcher icons (foreground and background).
    - Defined layout for the main activity with a simple "Hello World!" TextView.
    - Included mipmap resources for various screen densities.
    - Established color resources for the app's theme.
    - Configured themes and strings for the application.
    - Added file paths for external storage access.
    - Set up Gradle build scripts and properties for the Android project.
    - Implemented custom Gradle tasks for building Rust libraries.
    - Updated tauri configuration with a new product name.

---
+++ src-tauri/gen/android/app/src/main/java/ro/stuckinaloop/esp32_sms_sender_control_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package ro.stuckinaloop.esp32_sms_sender_control_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
